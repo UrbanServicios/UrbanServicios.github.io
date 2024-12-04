00960473e10e940a2318d636966051fb1fde3548
Fernandocruztovar332 <gtct2218@gmail.com>
2024-12-04 12:17:32 -0600

    camios en el port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const sql = require('mssql');
 
 // Crear la aplicación de Express
 const app = express();
-const port = 3000;
+const port = 30000;
 
 // Configurar middlewares
 app.use(bodyParser.json());
